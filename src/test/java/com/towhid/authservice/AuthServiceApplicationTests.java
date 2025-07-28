b5544ddecd3eee3c013491347b006b05e6c4f9e3
t0whid <towhid.hasan004@gmail.com>
2025-07-28 13:23:17 +0600

    Inital Commit

---
+++ src/test/java/com/towhid/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.towhid.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
