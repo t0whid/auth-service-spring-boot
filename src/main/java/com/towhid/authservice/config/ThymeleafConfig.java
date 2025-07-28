de345effeb0d076a26d9fd893c43ccf804fe3a59
t0whid <towhid.hasan004@gmail.com>
2025-07-28 17:40:32 +0600

    Add ThymeleafConfig and update pom.xml for Thymeleaf support

---
+++ src/main/java/com/towhid/authservice/config/ThymeleafConfig.java
@@ -0,0 +1,32 @@
+package com.towhid.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    @Primary
+    public TemplateEngine emailTemplateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(emailTemplateResolver());
+        return templateEngine;
+    }
+
+    private ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
+        templateResolver.setOrder(1);
+        return templateResolver;
+    }
+}
